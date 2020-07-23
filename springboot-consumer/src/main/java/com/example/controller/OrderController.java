384d72d632231dd632832b3ff3d3878fc0b6368a
zhangtao <miss_null@163.com>
2020-07-23 11:03:45 +0800

    1、接口优化

--- springboot-consumer/src/main/java/com/example/controller/OrderController.java
+++ springboot-consumer/src/main/java/com/example/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.example.controller;
 
 import com.example.entity.User;
 import com.example.service.UserClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Api(tags = "用户模块")
 @Controller
 @RequestMapping("/order")
 public class OrderController {
@@ -16,7 +21,8 @@ public class OrderController {
     @Autowired
     private UserClient userClient;
 
-
+    @ApiOperation(value = "获取某个用户详细信息", notes = "通过id查询用户")
+    @ApiImplicitParam(name = "userId", value = "用户的唯一标识", required = true, dataType = "int")
     @GetMapping("/toUser/{userId}")
     public ResponseEntity toUser(@PathVariable("userId") Integer userId){
         User user = userClient.getUserById(userId);
